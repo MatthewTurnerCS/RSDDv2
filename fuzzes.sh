ca5141a840a0d05434b03d0b00964e9e42772da7
Sam Olds <sam@spacemonkey.com>
2017-04-06 16:14:16 -0600

    added timing to logs. now to install more solvers and run on cade

--- fuzzes.sh
+++ fuzzes.sh
@@ -10,11 +10,11 @@ mkdir -p logs
 echo -e "the generated formulas used by the solvers\n" > logs/fuzzes.log
 echo -e "an overview of the results of each solver\n" > logs/fuzzes_complete.log
 echo -e "results when a solver crashed or inconsistencies\n" > logs/fuzzes_error.log
-echo -e "iteration\tz3-crash\tz3-sat" >> logs/fuzzes_complete.log
-echo -e "iteration\tz3-crash\tz3-sat" >> logs/fuzzes_error.log
+echo -e "iteration,z3-crash,z3-sat,z3-time" >> logs/fuzzes_complete.log
+echo -e "iteration,z3-crash,z3-sat,z3-time" >> logs/fuzzes_error.log
 
 # iterate a bunch
-for i in {1..1}
+for i in {1..5}
 do
   # generate a new formula
   java -classpath src FPFuzzer > out.smt2
@@ -25,16 +25,16 @@ do
   cat out.smt2 >> logs/fuzzes.log
 
   # run the solvers on the newly generated formulas
-  z3Out="$(./solvers/z3/bin/z3 -smt2 out.smt2)" # "sat" or "unsat"
+  z3SatAndTime="$(command time bash -c './solvers/z3/bin/z3 -smt2 out.smt2; echo ,' 2>&1 | xargs)"
   z3Res=$? # process ran successfully (0) or crashed (anything else)
 
   # if there was an error or an inconsistency, log it in errors
   if [ $z3Res -ne 0 ]; then
-    echo -e "$i\t$z3Res\t$z3Out" >> logs/fuzzes_error.log
+    echo -e "$i,$z3Res,$z3SatAndTime" >> logs/fuzzes_error.log
   fi
 
   # log results for each run of the solver
-  echo -e "$i\t$z3Res\t$z3Out" >> logs/fuzzes_complete.log
+  echo -e "$i,$z3Res,$z3SatAndTime" >> logs/fuzzes_complete.log
 done
 
 # cleanup
